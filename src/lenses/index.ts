32732572b372b15696d9c5b5f60e09476fade694
moki <morozov.kirill.moki@gmail.com>
2023-12-11 18:46:44 +0400

    feat(lenses): last lense

--- src/lenses/index.ts
+++ src/lenses/index.ts
@@ -4,4 +4,6 @@ const first = <T>() => lensIndex<T>(0);
 
 const second = <T>() => lensIndex<T>(1);
 
-export {first, second};
+const last = <T>() => lensIndex<T>(-1);
+
+export {first, second, last};
