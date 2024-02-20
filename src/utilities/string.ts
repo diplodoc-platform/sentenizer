69a6a1ac663d5ccae49ad0552c18bc0697604560
3y3 <3y3@ya.ru>
2024-02-20 16:57:17 +0300

    fix: Handle line breaks

--- src/utilities/string.ts
+++ src/utilities/string.ts
@@ -1,4 +1,4 @@
-import {compose, invoker, juxt, allPass, identity, not, toLower, toUpper, Pred} from 'ramda';
+import { compose, invoker, juxt, allPass, identity, not, toLower, toUpper, Pred, match } from 'ramda';
 
 import {allEqual} from './list';
 
@@ -9,6 +9,7 @@ const notAlpha = compose(allEqual, juxt([toLower, toUpper]));
 const hasAlpha = compose(not, notAlpha);
 
 const startsWithLower = allPass([
+    compose(compose(not, match(/\n/)), charAt(0)) as Pred<any[]>,
     compose(compose(not, notAlpha), charAt(0)) as Pred<any[]>,
     compose(allEqual, juxt([identity, toLower]), charAt(0)) as Pred<any[]>,
 ]);
